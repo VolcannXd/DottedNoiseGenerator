2d777bd20eab46d7e87bc1120f80304fb0a007d2
Caesar_Arthur <arthurdetaille.pro@gmail.com>
2019-12-30 16:20:59 +0100

    working... but Tkinter is way to slow
    I think I will switch to PIL and simply display the generated image

--- DottedNoiseGenerator.py
+++ DottedNoiseGenerator.py
@@ -39,15 +39,56 @@ def RgbToHex(rgb) :
     return "#%02x%02x%02x" % rgb
 
 def drawNoise() :
+    print("Log: Action: Draw Noise")
     if _canvas != None :
-        noise = noiseManager.perlinNoise(512, [], 10, 15)
-        pixels = generateNoiseMap(noise)
+        noise = noiseManager.perlinNoise(
+            512,
+            [],
+            scaleScale.get(),
+            thresholdScale.get()
+        )
 
+        b_background = int(255 * invertion.get())
+
+        points = generate_points(noise)
+
+        # CLEAR CANVAS
+        _canvas.delete("all")
+        _canvas.create_rectangle(0, 0, noise.size + 5, noise.size + 5, fill=RgbToHex((b_background, b_background, b_background)))
+
+
+        for p in points :
+            r = noise.threshold
+            b = 255 - b_background
+            color = RgbToHex((b, b, b))
+            _canvas.create_oval(p.x - r, p.y - r, p.x + r, p.y + r, fill=color, outline=color)
+
+        """
         for y in range(0, noise.size) :
             for x in range(0, noise.size) :
                 index = (y * noise.size) + x
-                b = pixels[index].brightness
-                _canvas.create_rectangle(x, y, x, y, fill=b)
+                b = RgbToHex((255, 255, 255))
+
+                distances = []
+
+                for i in range (0, len(noise.points)) :
+                    xD = abs(noise.points[i].x - x)
+                    yD = abs(noise.points[i].y - y)
+                    
+                    distances.append(math.sqrt(xD * xD + yD * yD))
+
+                distance = threshold
+                for i in range(0, len(distances)) :
+                    distance = distance - distances[i]
+
+                color = (0, 0, 0)
+                if distance >= 0 :
+                    b = b_background - ((distance / 255) * threshold)
+                    color = (b, b, b)
+
+                _canvas.create_rectangle((x, y) * 2, fill = RgbToHex(color))
+        """
+
     else :
         print("err: _canvas type == none type!")
 
@@ -72,7 +113,11 @@ def newFile() :
 
    if prefs[4] == 0 : SeamlessChecker.select()
    else : SeamlessChecker.deselect()
+
+   drawNoise()
     
+def loadExampleNoise() :
+    print("Log: Action: Load Example Noise")
 
 root = Tk()
 root.title("Dotted noise generator")
@@ -82,9 +127,13 @@ print("Log: Frame has been created.")
 menubar = Menu(root)
 
 # FILE MENU
+menuOpenExamples = Menu(menubar, tearoff = 0)
+menuOpenExamples.add_command(label="noiseExample.noise", command=loadExampleNoise)
+
 menuFile = Menu(menubar, tearoff=0)
 menuFile.add_command(label="New", command=newFile)
 menuFile.add_command(label="Open")
+menuFile.add_cascade(label="Open Examples", menu=menuOpenExamples)
 menuFile.add_command(label="Save As")
 menuFile.add_separator()
 menuFile.add_command(label="Quit", command=root.destroy)
@@ -104,6 +153,7 @@ menubar.add_cascade(label="File", menu=menuFile)
 menubar.add_cascade(label="Help", menu=menuHelp)
 menubar.add_cascade(label="Image", menu=menuImage)
 root.config(menu=menubar)
+
 print("Log: Menubar has been created and setup.")
 
 # Noise Generation parameters
@@ -111,12 +161,16 @@ generationGroup = LabelFrame(root, text="Generation paramters")
 generationGroup.pack(side = RIGHT, fill="both", expand="yes", padx = 5, pady = 5)
 
 threshold = IntVar()
-thresholdScale = Scale(generationGroup, label="Threshold (in pixel's distance)", orient="horizontal", from_=1, to=500, resolution=1, length=450)
+thresholdScale = Scale(generationGroup, label="Threshold (in pixel's distance)", orient="horizontal", from_=1, to=500, resolution=1, length=450, variable=threshold)
 thresholdScale.pack()
 
 scale = IntVar()
-scaleScale = Scale(generationGroup, label="scale", orient="horizontal", from_=1, to=100, resolution=1, length=450)
+scaleScale = Scale(generationGroup, label="scale", orient="horizontal", from_=1, to=100, resolution=1, length=450, variable=scale)
 scaleScale.pack()
+
+invertion = DoubleVar()
+invertionScale = Scale(generationGroup, orient="horizontal", label="Brightness Multiplier", from_=0, to=1, resolution=0.01, length=450, variable=invertion)
+invertionScale.pack()
 
 isInvert = IntVar()
 InvertChecker = Checkbutton(generationGroup, text="invert noise", variable=isInvert)
@@ -126,7 +180,7 @@ isSeamless = IntVar()
 SeamlessChecker = Checkbutton(generationGroup, text="seamless noise", variable=isSeamless)
 SeamlessChecker.pack(side=RIGHT, padx = 25, pady = 5)
 
-generateButton = Button(generationGroup, text = "Generate Noise", command=debug)
+generateButton = Button(generationGroup, text = "Generate Noise", command=drawNoise)
 generateButton.pack(side=BOTTOM, padx = 5, pady = 5)
 
 thresholdScale.set(50)
@@ -139,5 +193,7 @@ _canvas = Canvas(root, width = 512, height = 512, bg = 'black')
 _canvas.pack(side = LEFT, padx =5, pady =5)
 
 print("Log: Canvas has been created and setup.")
+
+drawNoise()
 
 root.mainloop()
